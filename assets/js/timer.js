cca81d0a4704da71f41a0624006518664945e49f
Ryan Lee <ryanlee059@gmail.com>
2021-04-19 19:31:43 -0700

    Adding code for button functionality. Added Pause functionality (to be tested)

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -5,18 +5,20 @@ var longbreak = $('#breakselect').find(':selected').val();
 var setTime;
 var minutes = Math.floor((setTime % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((setTime % (1000 * 60)) / 1000);
-
+var pause = false;
 
 function worktimer() {
     setTime = worktime;
     var pTimer = setInterval(function (){
-        setTime--;
-        $('#timerDisplay').html(`${minutes} : ${seconds}`);
-        if (setTime === 0) {
-            clearInterval(pTimer);
-            pomodoro++;
-            breaktimer();
-        }
+        if (!pause) {
+            setTime--;
+            $('#timerDisplay').html(`${minutes} : ${seconds}`);
+            if (setTime === 0) {
+                clearInterval(pTimer);
+                pomodoro++;
+                breaktimer();
+            }
+        }   
     }, 1000);
 };
 
@@ -24,42 +26,62 @@ function breaktimer() {
     if (pomodoro === 8) {
         setTime = longbreak;
         var pTimer = setInterval(function (){
-            setTime--;
-            $('#timerDisplay').html(`${minutes} : ${seconds}`);
-            if (setTime === 0) {
-                clearInterval(pTimer);
-                pomodoro = 0;
+            if (!pause) {
+                setTime--;
+                $('#timerDisplay').html(`${minutes} : ${seconds}`);
+                if (setTime === 0) {
+                    clearInterval(pTimer);
+                    pomodoro = 0;
 
+                }
             }
         }, 1000);
     } else if (pomodoro === 4) {
         setTime = longbreak;
         var pTimer = setInterval(function (){
-            setTime--;
-            $('#timerDisplay').html(`${minutes} : ${seconds}`);
-            if (setTime === 0) {
-                clearInterval(pTimer);
-                worktimer;
-                pomodoro = 0;
+            if (!pause) {
+                setTime--;
+                $('#timerDisplay').html(`${minutes} : ${seconds}`);
+                if (setTime === 0) {
+                    clearInterval(pTimer);
+                    worktimer;
+                    pomodoro = 0;
+                }
             }
         }, 1000);
     } else {
         setTime = shortbreak;
         var pTimer = setInterval(function (){
-            setTime--;
-            $('#timerDisplay').html(`${minutes} : ${seconds}`);
-            if (setTime === 0) {
-                clearInterval(pTimer);
-                worktimer;
+            if (!pause) {
+                setTime--;
+                $('#timerDisplay').html(`${minutes} : ${seconds}`);
+                if (setTime === 0) {
+                    clearInterval(pTimer);
+                    worktimer;
+                }
             }
         }, 1000);
     }
 };
 
-$('#start-button').on('click', function() {
-    if () {
-
-    } else {
-        worktimer();
-    }
+$('.start').on('click', function(e){
+    $('#play-pause').removeClass('start').addClass('pause');
+    worktimer();
 });
+
+$('.pause').on('click', function(e) {
+    e.preventDefault();
+    pause = true;
+    $('#play-pause').removeClass('pause').addClass('resume');
+});
+
+$('.resume').on('click', function(e){
+    e.preventDefault();
+    pause = false;
+    $('#play-pause').removeClass('resume').addClass('pause');
+});
+
+$('.stop').on('click', function(e){
+    e.preventDefault();
+    $('#play-pause').removeClass('pause resume').addClass('start');
+})
