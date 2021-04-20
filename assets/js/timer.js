2b15117d00876df5cad5b38bb7d4ce4ce8ff2f0f
Ryan Lee <ryanlee059@gmail.com>
2021-04-19 21:47:36 -0700

    Debugging 2: Debug harder

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -7,20 +7,21 @@ var minutes = Math.floor((setTime % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((setTime % (1000 * 60)) / 1000);
 var pause = false;
 var loop = 0;
+var timer;
 
 function worktimer() {
     setTime = worktime;
-    var pTimer = setInterval(function (){
+    var pTimer = debounce(function() {
         if (!pause) {
             setTime--;
             $('#timerDisplay').html(`${minutes} : ${seconds}`);
             if (setTime === 0) {
                 clearInterval(pTimer);
                 pomodoro++;
-                breaktimer();
+                // breaktimer();
             }
         }   
-    }, 1000);
+    }, worktime)
 };
 
 function breaktimer() {
@@ -86,8 +87,10 @@ $('.resume').on('click', function(e){
 $('.stop').on('click', function(e){
     e.preventDefault();
     $('#play-pause').removeClass('pause resume').addClass('start');
+    clearInterval(timer);
 })
 
 
 
-var pTimer = setInterval(setTime, 1000);
+
+  
